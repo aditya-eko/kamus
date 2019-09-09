1bec70825bc7477abe6337865ad3e4a4db75407e
aditya-eko <ekoagusaditya@gmail.com>
2019-09-09 15:34:57 +0700

    menambahkan file coba.py

--- coba.py
+++ coba.py
@@ -1,9 +1,2 @@
 from kamus import mahasiwa
 
-mahasiwa["nama"]="eko agus aditya"
-mahasiwa["asal"]="brebes"
-mahasiwa["fakultas"]="teknik informatika"
-mahasiwa["universitas"]="universitas teknologi jogjakarta"
-
-print("ini adalah mahasiswa {}".format(mahasiwa.items()))
-
