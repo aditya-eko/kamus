c0f652bb466957c8744a0c098c29c2bf42d8f7c6
aditya-eko <ekoagusaditya@gmail.com>
2019-09-09 15:50:15 +0700

    menambahkan file coba dan coba2.py

--- coba.py
+++ coba.py
@@ -1,2 +1,9 @@
 from kamus import mahasiwa
 
+mahasiwa["nama"]="eko agus aditya"
+mahasiwa["asal"]="brebes"
+mahasiwa["fakultas"]="teknik informatika"
+mahasiwa["universitas"]="universitas teknologi jogjakarta"
+
+print(mahasiwa.items())
+
--- coba2.py
+++ coba2.py
@@ -1,3 +1,20 @@
 from coba import mahasiwa
 
 print(mahasiwa)
+
+
+
+mahasiwa2={"no absen":"34",
+           "orangtua":"wustadi"
+
+           }
+# apabila mahasiswa di update dengan mahasiswa 2 maka mahasiwa akan di tambahkan dengan mahasiswa2
+# mahasiwa.update(mahasiwa2)
+# print(mahasiwa)
+
+# mahasiwa2.update(mahasiwa)
+# print(mahasiwa2)
+
+mahasiwa.update({"nama":"eko aditya",
+                 "kelahiran":"agustus"})
+print(mahasiwa)
