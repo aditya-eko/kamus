ba38cd19c8e4f853dc3ee87b5217b8206a404a8e
aditya-eko <ekoagusaditya@gmail.com>
2019-09-09 14:04:45 +0700

    menambahkan file .gitignore dan coba.py

---
+++ coba.py
@@ -0,0 +1,8 @@
+from kamus import mahasiwa
+
+mahasiwa["nama"]="eko agus aditya"
+mahasiwa["asal"]="brebes"
+mahasiwa["fakultas"]="teknik informatika"
+mahasiwa["universitas"]="universitas teknologi jogjakarta"
+
+print(mahasiwa.items())
